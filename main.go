c86d6a2dcf5e7ca4c51813e4454f60d71de911ae
Swaye Chateau <swaye@kitechsoftware.com>
2023-10-20 12:17:31 +0100

    fixed issue with port

--- main.go
+++ main.go
@@ -12,6 +12,6 @@ func main() {
 	fs := http.FileServer(http.Dir("./templates/static"))
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
 	routes.AppHandler("/", routes.SiteHandler)
-	log.Printf("Server started on port 8081")
-	log.Fatal(http.ListenAndServe(":8081", nil))
+	log.Printf("Server started on port 8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
