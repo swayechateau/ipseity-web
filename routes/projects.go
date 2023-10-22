64c2245025c7714005e356cd5b359e7ad6443087
Swaye Chateau <swaye@kitechsoftware.com>
2023-10-22 01:18:09 +0100

    WIP: projects

--- routes/projects.go
+++ routes/projects.go
@@ -8,8 +8,10 @@ import (
 func ProjectsHandler(w http.ResponseWriter, r *http.Request) {
 	log.Printf("Project Handler")
 	data := pageDataTemplate()
+	projects := ConvertApiProjects(state.Projects.All)
 	data.Hero.Image = "https://swayechateau.com/media/image/futaba-computer.jpg"
 	data.Hero.Title = "Projects"
+	data.Projects = &projects
 	RenderPage(w, "projects", data)
 }
 
