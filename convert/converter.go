73dcc808f1001f7b04051416ee85b013ece8c727
Swaye Chateau <swaye@kitechsoftware.com>
2023-10-25 15:21:23 +0100

    migrated to independant go package

--- convert/converter.go
+++ convert/converter.go
@@ -1,15 +1,14 @@
 package convert
 
 import (
-	"encoding/json"
-	"fmt"
 	"html/template"
-	"ipseity-web/convert/editorjs"
+
+	"github.com/swayechateau/solemnity-editorjs-go/editorjs"
 )
 
 func EditorJsToHtml(b interface{}) template.HTML {
 	var html template.HTML
-	editorjs, err := ConvertToEditorJs(b)
+	editorjs, err := editorjs.ConvertToEditorJs(b)
 	if err != nil {
 		return html
 	}
@@ -26,33 +25,4 @@ func ConvertToHtml(from string, data interface{}) template.HTML {
 	}
 
 	return ""
-}
-
-func ConvertToEditorJs(data interface{}) (editorjs.EditorJs, error) {
-	var parsedData editorjs.JsonEditorJs
-	var editorJs editorjs.EditorJs
-	var err error
-	var bytes []byte
-	bytes, err = toJson(data)
-	if err != nil {
-		fmt.Println("Error:", err)
-		return editorJs, err
-	}
-	err = json.Unmarshal(bytes, &parsedData)
-	if err != nil {
-		fmt.Println("Error:", err)
-		return editorJs, err
-	}
-	editorJs = parsedData.ToEditorJs()
-	return editorJs, nil
-}
-
-func toJson(p interface{}) ([]byte, error) {
-	bytes, err := json.Marshal(p)
-	if err != nil {
-		fmt.Println(err)
-		return bytes, err
-	}
-
-	return bytes, nil
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module ipseity-web
 
 go 1.21.1
+
+require github.com/swayechateau/solemnity-editorjs-go v0.0.0-20231025084805-e419a1b34f14 // indirect
